0e68c714c257807b2419d399f1ae8c03e616f394
dio22r <dio.22ratar@gmail.com>
2022-03-19 11:52:51 +0800

    move: profile gereja

--- app/Http/Controllers/Gereja/ProfileGerejaController.php
+++ app/Http/Controllers/Gereja/ProfileGerejaController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Gereja;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\GerejaRequest;
 use App\Models\MhGembala;
 use App\Models\MhWilayah;
