08d009195d35ca417733f23498f0e7b86478cd09
dio22r <dio.22ratar@gmail.com>
2022-03-27 15:58:23 +0800

    fix: overflow name

--- resources/views/panel/navbar.blade.php
+++ resources/views/panel/navbar.blade.php
@@ -14,18 +14,20 @@
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user fa-fw"></i> {{ Auth::user()->name }}
+                <i class="fas fa-cog fa-fw"></i>
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li>
-                    <a class="dropdown-item" href="{{ route('account') }}">My Account</a>
+                    <a class="dropdown-item" href="{{ route('account') }}">
+                        <i class="fas fa-user fa-fw"></i> {{ Auth::user()->name }}
+                    </a>
                 </li>
                 <li>
                     <hr class="dropdown-divider" />
                 </li>
                 <li>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
+                    <a class="dropdown-item text-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
                     </a>
 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
