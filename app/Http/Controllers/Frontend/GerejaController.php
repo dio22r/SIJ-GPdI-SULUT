61fdb319e505dd28741b28ff1701a427fb4c3298
dio22r <dio.22ratar@gmail.com>
2022-05-20 15:45:26 +0800

    fix: gereja & wilayah frontend

---
+++ app/Http/Controllers/Frontend/GerejaController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\MhGereja;
+use Illuminate\Http\Request;
+
+class GerejaController extends Controller
+{
+    public function index(Request $request)
+    {
+        $listGereja = MhGereja::with("MhWilayah", "MhGembala")
+            ->where("name", "like", "%" . $request->search . "%")
+            ->orderBy("name", "asc")
+            ->paginate(20);
+
+        return view("frontend.gereja.index", [
+            'listGereja' => $listGereja,
+            "search" => $request->search
+        ]);
+    }
+
+    public function show(Request $request, String $slug)
+    {
+        $gereja = MhGereja::with("MhWilayah", "MhGembala")
+            ->where("slug", "=", $slug)
+            ->firstOrFail();
+
+        return view("frontend.gereja.show", [
+            'gereja' => $gereja
+        ]);
+    }
+}
